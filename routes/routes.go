e329b478a0277074780ab9cb43a70e95bade98d8
taiki-nd <taiki.nd.real@gmail.com>
2022-04-29 22:42:41 +0900

    controllerの作成

--- routes/routes.go
+++ routes/routes.go
@@ -1,9 +1,11 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"admin_app_go/controllers"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func Routes(app *fiber.App) {
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
-	})
+	app.Get("/", controllers.Hello)
 }
