65d44d4617a499ce1c90502aa4abf76388d3dfbe
taiki-nd <taiki.nd.real@gmail.com>
2022-05-01 23:13:36 +0900

    logout機能の追加

--- routes/routes.go
+++ routes/routes.go
@@ -10,4 +10,5 @@ func Routes(app *fiber.App) {
 	app.Post("/api/register", controllers.Register)
 	app.Post("/api/login", controllers.Login)
 	app.Get("/api/user", controllers.User)
+	app.Post("api/logout", controllers.Logout)
 }
