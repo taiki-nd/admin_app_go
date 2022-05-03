9f5c862cb2441be018e5b41186f8b9011e76f560
taiki-nd <taiki.nd.real@gmail.com>
2022-05-03 17:11:56 +0900

    userモデルの修正

--- models/user.go
+++ models/user.go
@@ -1,15 +1,19 @@
 package models
 
 import (
+	"time"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
-	Model
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Email     string `json:"email" gorm:"unique"`
-	Password  []byte `json:"-"`
+	Id        uint      `json:"id" gorm:"primarykey"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email" gorm:"unique"`
+	Password  []byte    `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 func (user *User) SetPassword(password string) {
