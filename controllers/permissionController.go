15aac1450a7dec22251112d66b1f197dbd4b3ed5
taiki-nd <taiki.nd.real@gmail.com>
2022-05-06 23:07:50 +0900

    controller

---
+++ controllers/permissionController.go
@@ -0,0 +1,18 @@
+package controllers
+
+import (
+	"admin_app_go/db"
+	"admin_app_go/models"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func PermissionIndex(c *fiber.Ctx) error {
+	var permissions []models.Permission
+	db.DB.Preload("Role").Find(&permissions)
+
+	log.Println("show all users")
+
+	return c.JSON(permissions)
+}
