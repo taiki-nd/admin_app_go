f163532a44a3e270c0c11082e7dc18832d428dc7
taiki-nd <taiki.nd.real@gmail.com>
2022-05-09 22:16:31 +0900

    修正

--- models/order.go
+++ models/order.go
@@ -5,13 +5,13 @@ import (
 )
 
 type Order struct {
-	Id        uint        `json:"id" gorm:"primarykey"`
-	FirstName string      `json:"first_name"`
-	LastName  string      `json:"last_name"`
-	Email     string      `json:"email" gorm:"unique"`
-	OrderItem []OrderItem `json:""order_item gorm:"foreignKey:OrderId"`
-	CreatedAt time.Time   `json:"created_at"`
-	UpdatedAt time.Time   `json:"updated_at"`
+	Id         uint        `json:"id" gorm:"primarykey"`
+	FirstName  string      `json:"first_name"`
+	LastName   string      `json:"last_name"`
+	Email      string      `json:"email" gorm:"unique"`
+	OrderItems []OrderItem `json:"order_items" gorm:"foreignKey:OrderId"`
+	CreatedAt  time.Time   `json:"created_at"`
+	UpdatedAt  time.Time   `json:"updated_at"`
 }
 
 type OrderItem struct {
